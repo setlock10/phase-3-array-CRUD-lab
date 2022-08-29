c2fbcf6d6a0209434f36e9313dda9ac9b6609512
John J Setlock II <johnsetlock@gmail.com>
2022-08-29 07:53:15 -0500

    Completed Assignment

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,39 @@
 def create_an_empty_array
+    array=[]
+    return array
   
 end
 
 def create_an_array
+    array=[1,2,3,4]
+    return array
   
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  return array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    return array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+    return array.pop 
 end
 
 def remove_element_from_start_of_array(array)
-  
+    return array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    return array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    return array.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  return array.last
 end
